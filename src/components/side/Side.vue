9f39446c9aa8a7320756b5ebe0daba751e4cae51
Zengin <xp195107997@163.com>
2023-04-22 20:43:33 +0800

    aside

--- src/components/side/Side.vue
+++ src/components/side/Side.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col w-72 h-screen bg-indigo-100 border-r-2 border-indigo-500">
     <div class="basis-11/12">
-      <div class="rounded-md m-4 border-dotted border-4 border-indigo-600 text-center">
+      <div class="cursor-pointer rounded-md m-4 border-dotted border-4 border-indigo-600 text-center">
         New Chat
       </div>
-      <div class="rounded-md m-4 p-2 border-solid border-2 border-indigo-600">
-
-        <span>typescript 封装</span>
+      <div class="flex cursor-pointer content-center items-center rounded-md m-4 p-2 border-solid border-2 border-indigo-600">
+        <el-icon><chat-dot-square/></el-icon>
+        <span class="ml-2 overflow-hidden text-ellipsis whitespace-nowrap">typescript 封装一个axios请求</span>
       </div>
     </div>
     <div class="flex items-center h-20 h-12 border-t-2 border-indigo-500">
@@ -15,6 +15,7 @@
              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
              alt="">
         <span class="leading-10 ml-2 font-bold">name</span>
+        <el-icon><Setting/></el-icon>
       </div>
     </div>
   </div>
